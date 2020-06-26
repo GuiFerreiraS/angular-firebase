2b8836eb6690c85b853cb14319ca4dfc1ac56118
Guilherme Ferreira <guilherme12ferreirasouza@gmail.com>
2020-06-26 11:12:06 -0300

    creando metodos

--- angular-firebase-tasks/src/app/task.service.ts
+++ angular-firebase-tasks/src/app/task.service.ts
@@ -1,6 +1,7 @@
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Injectable } from '@angular/core';
 import { Task } from './models/task.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +20,29 @@ export class TaskService {
   private setTasks(): void {
     this.tasks = this.db.collection<Task>('/tasks');
   }
+
+  create(task: Task): Promise<void> {
+    const uid = this.db.createId();
+    return this.tasks.doc<Task>(uid)
+      .set({
+        uid,
+        title: task.title,
+        done: false
+      });
+  }
+
+  updade(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .update(task);
+  }
+
+  delete(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .delete();
+  }
+
+  get(uid: string): Observable<Task> {
+    return this.tasks.doc<Task>(uid).valueChanges();
+  }
+  
 }
